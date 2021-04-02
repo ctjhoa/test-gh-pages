342d37ac737805d0e5d68db535c37740df24c5fa
Camille TJHOA <camille.tjhoa@outlook.com>
2021-04-02 20:54:07 +0000

    Upgrade

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,4 @@ NEW_DIST="dist.$(git rev-parse HEAD)"
 cp -r dist "$NEW_DIST"
 git add "$NEW_DIST"
 git commit --allow-empty-message -m ''
-git co gh-pages
-git cherry-pick "$(git rev-parse HEAD)"
-git push origin gh-pages
+git checkout gh-pages -- "$NEW_DIST"
