9a77ed308e2c47fc294c30ece80ee15ee8ed47dc
Camille TJHOA <camille.tjhoa@outlook.com>
2021-04-02 21:04:26 +0000

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-NEW_DIST="dist.$(git rev-parse HEAD)"
+GIT_REF=$(git rev-parse HEAD)
+NEW_DIST="dist.$GIT_REF"
 cp -r dist "$NEW_DIST"
+git checkout gh-pages
 git add "$NEW_DIST"
-git commit --allow-empty-message -m ''
-git checkout gh-pages -- "$NEW_DIST"
+git commit -m "Release $GIT_REF"
+git push origin gh-pages
